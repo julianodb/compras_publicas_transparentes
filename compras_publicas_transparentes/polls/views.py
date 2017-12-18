63edbec34af843832e95d7a72afdc9b76b5ed94e
Juliano Barboza <julianodawid@gmail.com>
2017-12-18 19:17:05 -0300

    implements polls index that displays latest 5 polls

--- compras_publicas_transparentes/polls/views.py
+++ compras_publicas_transparentes/polls/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    return HttpResponse(latest_questions)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
