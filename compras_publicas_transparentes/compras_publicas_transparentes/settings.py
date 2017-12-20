f996a103ce2ee32862a5a8854dbae0c2fa2f1070
Juliano Barboza <julianodawid@gmail.com>
2017-12-20 12:00:05 -0300

    adds polls css

--- compras_publicas_transparentes/compras_publicas_transparentes/settings.py
+++ compras_publicas_transparentes/compras_publicas_transparentes/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
-TEST_OUTPUT_VERBOSE = True
+TEST_OUTPUT_VERBOSE = 2
 TEST_OUTPUT_DESCRIPTIONS = True
 TEST_OUTPUT_DIR = 'xmlrunner'
