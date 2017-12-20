df04919610e33a284cbc5dc629641ab12a6c37c8
Juliano Barboza <julianodawid@gmail.com>
2017-12-20 18:55:19 -0300

    adds name atribute test case for CompraPublica

--- compras_publicas_transparentes/mercadopublico_api/models.py
+++ compras_publicas_transparentes/mercadopublico_api/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+import requests
 
-# Create your models here.
+def getCompraPublica(code):
+    return response
+
 class CompraPublica(models.Model):
     name = models.CharField(max_length=200)
+    def __init__(self,code=None,request_method=requests):
+        if not code:
+            super().__init__()
+            return
+        response = request_method.get('http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.json?codigo={}&ticket=34EA724F-17C8-462E-B23B-4A92B3A2F622'.format(code)).json()
+        self.name = response['Listado'][0]['Nombre']
+
 #    pub_date = models.DateTimeField('date published')
 #
 #    def was_published_recently(self):
--- compras_publicas_transparentes/mercadopublico_api/tests.py
+++ compras_publicas_transparentes/mercadopublico_api/tests.py
@@ -8,6 +8,96 @@ class CompraPublicaModelTests(TestCase):
         """Model CompraPublica exists, is installed and migrated."""
         pass
 
+    def test_empty_comprapublica(self):
+        """CompraPublica object created with no arguments is empty"""
+        self.assertIs(CompraPublica().name,"")
+
+    def test_correct_atribute_names(self):
+        """Test expected atributes names for CompraPublica api"""
+        class fake_response():
+            def json(self):
+                return {"Cantidad":1,
+                    "FechaCreacion":"2017-12-20T13:22:22.64",
+                    "Version":"v1",
+                    "Listado":[{"Codigo":"2097-241-SE14",
+                        "Nombre":"Insumos dentales especialidades",
+                        "CodigoEstado":6,
+                        "Estado":"Aceptada",
+                        "CodigoLicitacion":"2097-165-L113",
+                        "Descripcion":"Insumos dentales especialidades DESDE 2097-165-L113\r\nDENTAL ESPECIALIDADES",
+                        "CodigoTipo":"8",
+                        "Tipo":"SE",
+                        "TipoMoneda":"CLP",
+                        "CodigoEstadoProveedor":4,
+                        "EstadoProveedor":"Aceptada",
+                        "Fechas":{"FechaCreacion":"2014-01-30T09:00:01.96",
+                            "FechaEnvio":"2014-01-30T09:36:28",
+                            "FechaAceptacion":"2014-02-02T23:58:56.63",
+                            "FechaCancelacion":None,
+                            "FechaUltimaModificacion":"2014-01-30T09:11:00"},
+                        "TieneItems":"1",
+                        "PromedioCalificacion":3.9333333333333331,
+                        "CantidadEvaluacion":6,
+                        "Descuentos":0.0,
+                        "Cargos":0.0,
+                        "TotalNeto":35000.0,
+                        "PorcentajeIva":19.0,
+                        "Impuestos":6650.0,
+                        "Total":41650.0,
+                        "Financiamiento":"",
+                        "Pais":"CL",
+                        "TipoDespacho":"7",
+                        "FormaPago":"2",
+                        "Comprador":{"CodigoOrganismo":"7398",
+                            "NombreOrganismo":"Hospital Arauco",
+                            "RutUnidad":"61.602.209-1",
+                            "CodigoUnidad":"3092",
+                            "NombreUnidad":"Hospital Arauco",
+                            "Actividad":"",
+                            "DireccionUnidad":"Caupolican S/N",
+                            "ComunaUnidad":"Arauco",
+                            "RegionUnidad":"Región del Biobío ",
+                            "Pais":"CL",
+                            "NombreContacto":"Yohana Lidia Maldonado Medina",
+                            "CargoContacto":"Administrativo Abastecimiento",
+                            "FonoContacto":"56-41-2725940",
+                            "MailContacto":"maldonadoyohana1@gmail.com"},
+                        "Proveedor":{"Codigo":"1316979",
+                            "Nombre":"COMERCIALIZADORA ILHABELLA EIRL",
+                            "Actividad":"VENTA AL POR MAYOR DE OTROS PRODUCTOS N.C.P.| VENT",
+                            "CodigoSucursal":"717107",
+                            "NombreSucursal":"COMERCIALIZADORA ILHABELLA EIRL",
+                            "RutSucursal":"76.242.192-5",
+                            "Direccion":"AV. PAUL HARRIS 1464",
+                            "Comuna":"Las Condes",
+                            "Region":"Región Metropolitana de Santiago",
+                            "Pais":"CL",
+                            "NombreContacto":"MARLENE BEATRIZ FLORES PATIÑO",
+                            "CargoContacto":"SANTIAGO",
+                            "FonoContacto":"56-2-2122966",
+                            "MailContacto":"marlenebeatrizflores@gmail.com"},
+                        "Items":{"Cantidad":1,
+                            "Listado":[{"Correlativo":1,
+                                "CodigoCategoria":30201900,
+                                "Categoria":"Artículos para estructuras,obras y construcciones / Estructuras prefabricadas / Construcciones médicas prefabricadas",
+                                "CodigoProducto":30201903,
+                                "Producto":"Unidades dentales",
+                                "EspecificacionComprador":"Ultracal",
+                                "EspecificacionProveedor":" Ultracal.ULTRADENT. PRODUCTO AMERICANO. JERINGA 3 GRS. KIT X 4 UNIDADES.  FECHA DE VENCIMIENTO 2018. SE ADJUNTA CATALOGO DE PRODUCTO. INCLUY FLETE. DESPACHO 24 HRS. PUESTO EN BODEGAS DE HOSPITAL DE ARAUCO.",
+                                "Cantidad":1.0,
+                                "Unidad":None,
+                                "Moneda":"CLP",
+                                "PrecioNeto":35000.0,
+                                "TotalCargos":0.0,
+                                "TotalDescuentos":0.0,
+                                "TotalImpuestos":0.0,
+                                "Total":35000.0}]}}]}
+        class fake_request():
+            def get(code):
+                return fake_response()
+        cp = CompraPublica('fakecode',fake_request)
+        self.assertEqual(cp.name,"Insumos dentales especialidades")
+
 #    def test_was_published_recently_with_future_questions(self):
 #        """
 #        was_published_recently() returns False for questions whose pub_date
