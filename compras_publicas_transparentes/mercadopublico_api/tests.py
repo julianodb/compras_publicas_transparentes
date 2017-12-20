d03c1af53ce81e95021fbc2b15981791b4104cf9
Juliano Barboza <julianodawid@gmail.com>
2017-12-20 13:19:57 -0300

    adds test for CompraPublica model existance

--- compras_publicas_transparentes/mercadopublico_api/tests.py
+++ compras_publicas_transparentes/mercadopublico_api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import CompraPublica
+
+class CompraPublicaModelTests(TestCase):
+
+    def test_exists(self):
+        """Model CompraPublica exists, is installed and migrated."""
+        pass
+
+#    def test_was_published_recently_with_future_questions(self):
+#        """
+#        was_published_recently() returns False for questions whose pub_date
+#        is in the future.
+#        """
+#        time = timezone.now() + datetime.timedelta(days=30)
+#        future_question = Question(pub_date=time)
+#        self.assertIs(future_question.was_published_recently(), False)
+
