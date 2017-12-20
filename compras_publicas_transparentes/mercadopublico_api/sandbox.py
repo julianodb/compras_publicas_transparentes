36bfc2de9ec90c5f2a89d692e4ad700c710d6e47
Juliano Barboza <julianodawid@gmail.com>
2017-12-20 17:51:39 -0300

    adds sandbox, pip requests

---
+++ compras_publicas_transparentes/mercadopublico_api/sandbox.py
@@ -0,0 +1,4 @@
+import requests
+
+response = requests.get('http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.json?codigo=2097-241-SE14&ticket=34EA724F-17C8-462E-B23B-4A92B3A2F622')
+print(response.json())
