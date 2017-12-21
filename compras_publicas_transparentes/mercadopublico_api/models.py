4c6c7b2cb03ea0e8bb29d59b4a4ab1556d0d7d9d
Juliano Barboza <julianodawid@gmail.com>
2017-12-21 15:49:30 -0300

    refactors models.py

--- compras_publicas_transparentes/mercadopublico_api/models.py
+++ compras_publicas_transparentes/mercadopublico_api/models.py
@@ -1,67 +1,68 @@
+"""CompraPublica model"""
 from django.db import models
 import requests
 
 class CompraPublica(models.Model):
-    STATE_CODE_CHOICES = ( (4, "Enviada a Proveedor"),
-                           (6, "Aceptada"),
-                           (9, "Cancelada"),
-                           (12, "Recepción Conforme"),
-                           (13, "Pendiente de Recepcionar"),
-                           (14, "Recepcionada Parcialmente"),
-                           (15, "Recepcion Conforme Incompleta"),
+    STATE_CODE_CHOICES = ((4, "Enviada a Proveedor"),
+                          (6, "Aceptada"),
+                          (9, "Cancelada"),
+                          (12, "Recepción Conforme"),
+                          (13, "Pendiente de Recepcionar"),
+                          (14, "Recepcionada Parcialmente"),
+                          (15, "Recepcion Conforme Incompleta"),
                          )
-    TYPE_CODE_CHOICES = ( ("1","OC - Automática"),
-                          ("2",("D1 - Trato directo que genera Orden de Co"
+    TYPE_CODE_CHOICES = (("1", "OC - Automática"),
+                         ("2", ("D1 - Trato directo que genera Orden de Co"
                                 "mpra por proveedor único")),
-                          ("3",("C1 - Trato directo que genera Orden de Co"
+                         ("3", ("C1 - Trato directo que genera Orden de Co"
                                 "mpra por emergencia, urgencia e imprevist"
                                 "o")),
-                          ("4",("F3 - Trato directo que genera Orden de Co"
+                         ("4", ("F3 - Trato directo que genera Orden de Co"
                                 "mpra por confidencialidad")),
-                          ("5",("G1 - Trato directo que genera Orden de Co"
+                         ("5", ("G1 - Trato directo que genera Orden de Co"
                                 "mpra por naturaleza de negociación")),
-                          ("6","R1 - Orden de compra menor a 3UTM"),
-                          ("7","CA - Orden de compra sin resolución."),
-                          ("8","SE - Sin emisión automática"),
-                          ("9","CM - Convenio Marco"),
-                          ("10","FG"),
-                          ("11","TL"),
+                         ("6", "R1 - Orden de compra menor a 3UTM"),
+                         ("7", "CA - Orden de compra sin resolución."),
+                         ("8", "SE - Sin emisión automática"),
+                         ("9", "CM - Convenio Marco"),
+                         ("10", "FG"),
+                         ("11", "TL"),
                         )
-    CURRENCY_CHOICES = ( ("CLP","Peso Chileno"),
-                         ("CLF","Unidad de Fomento"),
-                         ("USD","Dólar Americano"),
-                         ("UTM","Unidad Tributaria Mensual"),
-                         ("EUR","Euro"),
+    CURRENCY_CHOICES = (("CLP", "Peso Chileno"),
+                        ("CLF", "Unidad de Fomento"),
+                        ("USD", "Dólar Americano"),
+                        ("UTM", "Unidad Tributaria Mensual"),
+                        ("EUR", "Euro"),
                        )
-    DELIVERY_TYPE_CHOICES = ( ("7", "Despachar a Dirección de envío"),
-                              ("9", "Despachar según programa adjuntado"),
-                              ("12", "Otra Forma de Despacho, Ver Instruc"),
-                              ("14", "Retiramos de su bodega"),
-                              ("20", ("Despacho por courier o encomienda a"
-                                      "érea")),
-                              ("21", ("Despacho por courier o encomienda t"
-                                      "errestre")),
-                              ("22", "A convenir"),
+    DELIVERY_TYPE_CHOICES = (("7", "Despachar a Dirección de envío"),
+                             ("9", "Despachar según programa adjuntado"),
+                             ("12", "Otra Forma de Despacho, Ver Instruc"),
+                             ("14", "Retiramos de su bodega"),
+                             ("20", ("Despacho por courier o encomienda a"
+                                     "érea")),
+                             ("21", ("Despacho por courier o encomienda t"
+                                     "errestre")),
+                             ("22", "A convenir"),
                             )
-    PAYMENT_TYPE_CHOICES = ( ("1", ("15 días contra la recepción de la fac"
-                                    "tura")),
-                             ("2", ("30 días contra la recepción de la fac"
-                                    "tura")),
-                             ("39", "Otra forma de pago"),
-                             ("46", ("50 días contra la recepción de la fa"
-                                     "ctura")),
-                             ("47", ("60 días contra la recepción de la fa"
-                                     "ctura")),
+    PAYMENT_TYPE_CHOICES = (("1", ("15 días contra la recepción de la fac"
+                                   "tura")),
+                            ("2", ("30 días contra la recepción de la fac"
+                                   "tura")),
+                            ("39", "Otra forma de pago"),
+                            ("46", ("50 días contra la recepción de la fa"
+                                    "ctura")),
+                            ("47", ("60 días contra la recepción de la fa"
+                                    "ctura")),
                            )
     code = models.CharField(max_length=50)
     name = models.CharField(max_length=200)
     state_code = models.PositiveSmallIntegerField(choices=STATE_CODE_CHOICES)
     state_name = models.CharField(max_length=100)
-    tender_code = models.CharField(max_length=50,null=True)
+    tender_code = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=500)
-    type_code = models.CharField(max_length=2,choices=TYPE_CODE_CHOICES)
+    type_code = models.CharField(max_length=2, choices=TYPE_CODE_CHOICES)
     type_name = models.CharField(max_length=10)
-    currency = models.CharField(max_length=3,choices=CURRENCY_CHOICES)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
     supplier_state_code = models.CharField(max_length=10)
     supplier_state = models.CharField(max_length=100)
     has_items = models.CharField(max_length=10)
@@ -81,15 +82,24 @@ class CompraPublica(models.Model):
                                     choices=PAYMENT_TYPE_CHOICES)
 
     @classmethod
-    def create(cls,code=None,request_method=requests):
+    def create(cls, code=None, request_method=requests):
+        """Creates a new CompraPublica with the given code
+
+        If an entry with the given code is not found in the database,
+        this method requests from the mercadopublico api.
+        If no code is given, an empty object is created.
+        Accepts a request_method for mocking the request to the api
+        """
         if not code:
             return cls()
         try:
             return cls.objects.get(code=code)
         except cls.DoesNotExist:
             new_object = cls()
-            req_url = 'http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.json?codigo={}&ticket=34EA724F-17C8-462E-B23B-4A92B3A2F622'
-            req_url = req_url.format(code)
+            ticket = '34EA724F-17C8-462E-B23B-4A92B3A2F622'
+            req_url = ('http://api.mercadopublico.cl/servicios/v1/publico/'
+                       'ordenesdecompra.json?codigo={c}&ticket={t}')
+            req_url = req_url.format(c=code, t=ticket)
             response = request_method.get(req_url).json()
             for _ in range(5):
                 while 'Codigo' in response:
@@ -122,31 +132,26 @@ class CompraPublica(models.Model):
             return new_object
 
     @classmethod
-    def get_last_five(cls,request_method=requests):
-        req_url = 'http://api.mercadopublico.cl/servicios/v1/publico/ordenesdecompra.json?estado=todos&ticket=34EA724F-17C8-462E-B23B-4A92B3A2F622'
+    def get_last_five(cls, request_method=requests):
+        """Returns list with last five CompraPublica found today
+
+        The order of the objects is umpredictable, so it may change
+        even if there is no new entry.
+        Also accepts a request_method for mocking.        
+        """
+        ticket = '34EA724F-17C8-462E-B23B-4A92B3A2F622'
+        req_url = ('http://api.mercadopublico.cl/servicios/v1/publico/orde'
+                   'nesdecompra.json?estado=todos&ticket={t}')
+        req_url = req_url.format(t=ticket)
         response = request_method.get(req_url).json()
         result = []
         for candidate in response['Listado'][:5]:
             result.append(CompraPublica.create(candidate['Codigo']))
-
         return result
-    
+
     def __repr__(self):
         result = 'CompraPublica('
-        for k,v in vars(self).items():
-            if not k.startswith('_'):
-                result += '{}={!r},'.format(k,v)
+        for key, value in vars(self).items():
+            if not key.startswith('_'):
+                result += '{}={!r},'.format(key, value)
         return result[:-1] + ')'
-
-
-        #return response['Listado'][:5]
-
-#    pub_date = models.DateTimeField('date published')
-#
-#    def was_published_recently(new_object):
-#        now = timezone.now()
-#        return now - datetime.timedelta(days=1) <= new_object.pub_date <= now
-#
-#    def __str__(new_object):
-#        return new_object.question_text
-
